a249dc1bef41e1c10acfe8762cfe9fbc3fd61ffe
Joe Sepi <sepi@joesepi.com>
2014-03-21 16:11:24 -0400

    jshint the things : models dir all done

--- src/models/boilerplate.js
+++ src/models/boilerplate.js
@@ -1,104 +1,100 @@
+nv.models.chartName = function () {
+    "use strict";
+    //============================================================
+    // Public Variables with Default Settings
+    //------------------------------------------------------------
 
-nv.models.chartName = function() {
-  "use strict";
-  //============================================================
-  // Public Variables with Default Settings
-  //------------------------------------------------------------
+    var margin = {
+        top: 30,
+        right: 10,
+        bottom: 10,
+        left: 10
+    },
+        width = 960,
+        height = 500,
+        color = nv.utils.getColor(d3.scale.category20c()
+            .range()),
+        dispatch = d3.dispatch('stateChange', 'changeState');
 
+    //============================================================
 
-  var margin = {top: 30, right: 10, bottom: 10, left: 10}
-    , width = 960
-    , height = 500
-    , color = nv.utils.getColor(d3.scale.category20c().range())
-    , dispatch = d3.dispatch('stateChange', 'changeState')
-    ;
+    //============================================================
+    // Private Variables
+    //------------------------------------------------------------
 
-  //============================================================
+    //============================================================
 
+    function chart(selection) {
+        selection.each(function (data) {
+            var availableWidth = width - margin.left - margin.right,
+                availableHeight = height - margin.top - margin.bottom,
+                container = d3.select(this);
 
-  //============================================================
-  // Private Variables
-  //------------------------------------------------------------
+            //------------------------------------------------------------
+            // Setup Scales
 
+            //------------------------------------------------------------
 
-  //============================================================
+            //------------------------------------------------------------
+            // Setup containers and skeleton of chart
 
+            var wrap = container.selectAll('g.nv-wrap.nv-chartName')
+                .data([data]);
+            var wrapEnter = wrap.enter()
+                .append('g')
+                .attr('class', 'nvd3 nv-wrap nv-chartName');
+            var gEnter = wrapEnter.append('g');
+            var g = wrap.select('g')
 
-  function chart(selection) {
-    selection.each(function(data) {
-      var availableWidth = width - margin.left - margin.right,
-          availableHeight = height - margin.top - margin.bottom,
-          container = d3.select(this);
+            gEnter.append('g')
+                .attr('class', 'nv-mainWrap');
 
+            wrap.attr('transform', 'translate(' + margin.left + ',' +
+                margin.top + ')');
 
-      //------------------------------------------------------------
-      // Setup Scales
+            //------------------------------------------------------------
 
+        });
 
-      //------------------------------------------------------------
+        return chart;
+    }
 
+    //============================================================
+    // Expose Public Variables
+    //------------------------------------------------------------
 
-      //------------------------------------------------------------
-      // Setup containers and skeleton of chart
+    chart.dispatch = dispatch;
 
-      var wrap = container.selectAll('g.nv-wrap.nv-chartName').data([data]);
-      var wrapEnter = wrap.enter().append('g').attr('class', 'nvd3 nv-wrap nv-chartName');
-      var gEnter = wrapEnter.append('g');
-      var g = wrap.select('g')
+    chart.options = nv.utils.optionsFunc.bind(chart);
 
-      gEnter.append('g').attr('class', 'nv-mainWrap');
+    chart.margin = function (_) {
+        if (!arguments.length) return margin;
+        margin.top = typeof _.top != 'undefined' ? _.top : margin.top;
+        margin.right = typeof _.right != 'undefined' ? _.right : margin.right;
+        margin.bottom = typeof _.bottom != 'undefined' ? _.bottom : margin.bottom;
+        margin.left = typeof _.left != 'undefined' ? _.left : margin.left;
+        return chart;
+    };
 
-      wrap.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    chart.width = function (_) {
+        if (!arguments.length) return width;
+        width = _;
+        return chart;
+    };
 
-      //------------------------------------------------------------
+    chart.height = function (_) {
+        if (!arguments.length) return height;
+        height = _;
+        return chart;
+    };
 
+    chart.color = function (_) {
+        if (!arguments.length) return color;
+        color = nv.utils.getColor(_)
+        return chart;
+    };
 
-
-
-    });
+    //============================================================
 
     return chart;
-  }
-
-
-  //============================================================
-  // Expose Public Variables
-  //------------------------------------------------------------
-
-
-  chart.dispatch = dispatch;
-
-  chart.options = nv.utils.optionsFunc.bind(chart);
-  
-  chart.margin = function(_) {
-    if (!arguments.length) return margin;
-    margin.top    = typeof _.top    != 'undefined' ? _.top    : margin.top;
-    margin.right  = typeof _.right  != 'undefined' ? _.right  : margin.right;
-    margin.bottom = typeof _.bottom != 'undefined' ? _.bottom : margin.bottom;
-    margin.left   = typeof _.left   != 'undefined' ? _.left   : margin.left;
-    return chart;
-  };
-
-  chart.width = function(_) {
-    if (!arguments.length) return width;
-    width = _;
-    return chart;
-  };
-
-  chart.height = function(_) {
-    if (!arguments.length) return height;
-    height = _;
-    return chart;
-  };
-
-  chart.color = function(_) {
-    if (!arguments.length) return color;
-    color = nv.utils.getColor(_)
-    return chart;
-  };
-
-  //============================================================
-
-
-  return chart;
 }
